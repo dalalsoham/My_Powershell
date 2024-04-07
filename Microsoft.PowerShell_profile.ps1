e920ba93939e51895758f79a151c4d3c4dcef2a2
mr_sd_jack_003 <barson0habra@gmail.com>
2024-04-07 11:32:14 +0530

    upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+ oh-my-posh init pwsh --config 'C:\Users\SOHAM DALAL\AppData\Local\Programs\oh-my-posh\themes\paradox.omp.json' | Invoke-Expression
+
+ set-PSReadLineOption -PredictionViewStyle ListView
