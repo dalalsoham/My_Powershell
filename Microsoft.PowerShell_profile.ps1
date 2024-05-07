774afcf6549c768fbe6d858d8e9a217fe08055f6
mr_sd_jack_003 <barson0habra@gmail.com>
2024-05-08 00:32:35 +0530

    new git config

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,55 +7,138 @@
  #adapter device connection 
  set-alias -name eth -value get-netadapter
 
- #git command shortcut all
- function goGoGadgetGit {
-    Param(
-      [Parameter(Mandatory = $true, Position = 0)]
-      [String]
-      $Cmd,
-  
-      [Parameter(Mandatory = $false, ValueFromRemainingArguments = $true)]
-      [String[]]
-      $Params
-    )
-  
-    Switch ($Cmd)
-    {
-      # status
-      's' { git status $Params }
-      #init
-      'i' {git init $Params }
-      #add
-      'a' {git add $Params }
-      #branch
-      'b' {git branch $Params }
-      #checkout
-      'ch' {git checkout $Params }
-      #clean
-      'cl' {git clean $Params }
-      #clone
-      'cln' {git clone $Params }
-      #commit
-      'cm' {git commit $Params }
-      #config
-      'cf' {git config $Params }
-      #log
-      'l' {git log $Params }
-      #merge
-      'm' {git merge $Params }
-      #pull
-      'pl' {git pull $Params }
-      #push
-      'pu' {git push $Params }
-      #remote
-      're' {git remote $Params }
-    }
-  }
-  Set-Alias g goGoGadgetGit
+ 
  
   #my important path shortcut
   function prod { cd "D:\projects_new" }
   function cloned { cd "D:\github_clone_in_d"}
+  function mywebsite { cd "D:\My_Portfolio" }
 
   #my powershell folder shortcut
   function power {cd "C:\Users\SOHAM DALAL\OneDrive\Documents\PowerShell"}
+
+  #npm shortcut
+  function NpmDev {
+
+    npm run dev
+  
+  }
+  
+  Set-Alias nd NpmDev
+
+  function NpmStart {
+
+    npm start
+  }
+
+  Set-Alias ns NpmStart
+
+  function NpmInstall {
+
+    npm install
+  }
+
+  Set-Alias nins NpmInstall
+
+ #git command shortcut all
+  function GitInit {
+    git init
+  }
+
+  Set-Alias gin GitInit
+
+  function GitStatus {
+    git status
+  }
+
+  Set-Alias gs GitStatus
+
+  function GitAdd {
+    git add .
+  }
+
+  Set-Alias gaa GitAdd
+
+  function GitBranch {
+    git branch
+  }
+
+  Set-Alias gb GitBranch
+
+  function GitCheckOut {
+    git checkout
+  }
+
+  Set-Alias gch GitCheckOut
+
+  function GitClean {
+    git clean
+  }
+
+  Set-Alias gcln GitClean
+
+  function GitCommit {
+    git commit -m
+  }
+
+  Set-Alias gcommit GitCommit
+
+  function GitConfig {
+    git config
+  }
+
+  Set-Alias gcon GitConfig
+
+  function Gitlog {
+    git log
+  }
+
+  Set-Alias glog GitLog
+
+  function GitMerge {
+    git merge
+  }
+
+  Set-Alias gmerge GitMerge
+
+  function GitPull {
+    git pull
+  }
+
+  Set-Alias gpl GitPull
+
+  function GitPush {
+    git push
+  }
+
+  Set-Alias gpu GitPush
+
+  function GitRemote {
+    git remote
+  }
+
+  Set-Alias gre GitRemote
+
+  function GitDiff {
+    git diff
+  }
+
+  Set-Alias gdi GitDiff
+
+  function GitRemoteAddOrigin {
+    git remote add origin 
+  }
+
+  Set-Alias gorigin GitRemoteAddOrigin
+
+  function GitPushUOriginMaster {
+    git push -u origin master
+  }
+
+  Set-Alias gmaster GitPushUOriginMaster
+
+  function GitPushUOriginMain {
+    git push -u origin main
+  }
+
+  Set-Alias gmain GitPushUOriginMain
