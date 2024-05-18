959d9b9fb636bc79aa638c53cadfae37cb364125
mr_sd_jack_003 <barson0habra@gmail.com>
2024-05-18 18:03:11 +0530

    change posh theme paradox to takuya

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
  #oh-my-posh config and file path
- oh-my-posh init pwsh --config 'C:\Users\SOHAM DALAL\AppData\Local\Programs\oh-my-posh\themes\paradox.omp.json' | Invoke-Expression
+ oh-my-posh init pwsh --config 'C:\Users\SOHAM DALAL\AppData\Local\Programs\oh-my-posh\themes\takuya.omp.json' | Invoke-Expression
 
 #Terminal icons
 Import-Module Terminal-Icons
